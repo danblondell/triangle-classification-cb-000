92ce8ce69846093157e27012feabba7c8832fc5f
Dan Blondell <danblondell@gmail.com>
2017-08-10 02:55:36 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @all_sides = [side1, side2, side3]
+  end
+
+  def kind
+    if @all_sides.any? { |side| side <= 0 }
+      raise TriangleError
+    elsif @all_sides.any? { |side| side >= (@all_sides.inject{|sum, side| sum + side} - side) }
+      raise TriangleError
+    elsif @all_sides.uniq.length == 1
+      :equilateral
+    elsif @all_sides.uniq.length == 2
+      :isosceles
+    elsif @all_sides.uniq.length == 3
+      :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
 end
